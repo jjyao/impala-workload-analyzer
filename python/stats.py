41b34c0156c65feed978347da573f79351ebdb94
jjyao <jeromeyjj@gmail.com>
2015-04-03 22:49:21 -0400

    histogram

--- python/stats.py
+++ python/stats.py
@@ -1,179 +1,201 @@
 import sys
+import numpy
 import pymongo
+from matplotlib import pyplot
 
-sum_num_joins = 0
-max_num_joins = 0
-min_num_joins = sys.maxint
+def hist(data, minval, maxval, xlabel, ylabel, title, output):
+    pyplot.clf()
+    min_num_bins = 10
+    max_num_bins = 10
+    step = max(1, (maxval - minval) / max_num_bins)
+    start = minval
+    stop = max(start + step * (min_num_bins + 1), maxval + step)
+    if isinstance(minval, int):
+        bins = range(start, stop, step)
+    else:
+        bins = numpy.arange(start, stop, step)
+    pyplot.hist(data, bins)
+    pyplot.xlabel(xlabel)
+    pyplot.ylabel(ylabel)
+    pyplot.title(title)
+    pyplot.tight_layout()
+    pyplot.savefig(output)
 
-sum_num_broadcast_joins = 0
-max_num_broadcast_joins = 0
-min_num_broadcast_joins = sys.maxint
-
-sum_num_partitioned_joins = 0
-max_num_partitioned_joins = 0
-min_num_partitioned_joins = sys.maxint
-
-sum_num_inner_joins = 0
-max_num_inner_joins = 0
-min_num_inner_joins = sys.maxint
-
-sum_num_tables = 0
-max_num_tables = 0
-min_num_tables = sys.maxint
-
-sum_num_hdfs_scans = 0
-max_num_hdfs_scans = 0
-min_num_hdfs_scans = sys.maxint
-
-sum_num_output_columns = 0
-max_num_output_columns = 0
-min_num_output_columns = sys.maxint
-
-sum_num_group_by_columns = 0
-max_num_group_by_columns = 0
-min_num_group_by_columns = sys.maxint
-
-sum_num_order_by_columns = 0
-max_num_order_by_columns = 0
-min_num_order_by_columns = sys.maxint
-
-sum_hdfs_scan_size = 0
-max_hdfs_scan_size = 0
-min_hdfs_scan_size = sys.maxint
-
-sum_num_from_subqueries = 0
-max_num_from_subqueries = 0
-min_num_from_subqueries = sys.maxint
-
-num_limit = 0
 
 db = pymongo.MongoClient().impala
 
 queries = db.queries.find()
 
+num_joins = []
+num_broadcast_joins = []
+num_partitioned_joins = []
+num_inner_joins = []
+
+num_tables = []
+num_hdfs_scans = []
+
+num_from_subqueries = []
+
+num_output_columns = []
+num_group_by_columns = []
+num_order_by_columns = []
+
+hdfs_scan_size = []
+
+runtime = []
+
+num_limit = 0
+
+num_queries = queries.count()
+
 for query in queries:
-    num_joins = db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN'}).count()
-    sum_num_joins += num_joins
-    max_num_joins = max(max_num_joins, num_joins)
-    min_num_joins = min(min_num_joins, num_joins)
+    num_joins.append(
+            db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN'}).count())
 
-    num_broadcast_joins = db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_impl': 'BROADCAST'}).count()
-    sum_num_broadcast_joins += num_broadcast_joins
-    max_num_broadcast_joins = max(max_num_broadcast_joins, num_broadcast_joins)
-    min_num_broadcast_joins = min(min_num_broadcast_joins, num_broadcast_joins)
+    num_broadcast_joins.append(
+            db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_impl': 'BROADCAST'}).count())
 
-    num_partitioned_joins = db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_impl': 'PARTITIONED'}).count()
-    sum_num_partitioned_joins += num_partitioned_joins
-    max_num_partitioned_joins = max(max_num_partitioned_joins, num_partitioned_joins)
-    min_num_partitioned_joins = min(min_num_partitioned_joins, num_partitioned_joins)
+    num_partitioned_joins.append(
+            db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_impl': 'PARTITIONED'}).count())
 
-    num_inner_joins = db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_type': 'INNER JOIN'}).count()
-    sum_num_inner_joins += num_inner_joins
-    max_num_inner_joins = max(max_num_inner_joins, num_inner_joins)
-    min_num_inner_joins = min(min_num_inner_joins, num_inner_joins)
+    num_inner_joins.append(
+            db.operators.find({'query_id': query['_id'], 'name': 'HASH JOIN', 'join_type': 'INNER JOIN'}).count())
 
-    hdfs_scan_size = db.operators.aggregate([
-        {'$match': {'query_id': query['_id'], 'name': 'SCAN HDFS'}},
-        {'$group': {'_id': None, 'size': {'$sum': '$size'}}},
-    ])['result'][0]['size']
-    sum_hdfs_scan_size += hdfs_scan_size
-    max_hdfs_scan_size = max(max_hdfs_scan_size, hdfs_scan_size)
-    min_hdfs_scan_size = min(min_hdfs_scan_size, hdfs_scan_size)
+    hdfs_scan_size.append(
+            db.operators.aggregate([
+                {'$match': {'query_id': query['_id'], 'name': 'SCAN HDFS'}},
+                {'$group': {'_id': None, 'size': {'$sum': '$size'}}},
+            ])['result'][0]['size'] / 1024 / 1024)
 
-    num_tables = query['num_tables']
-    sum_num_tables += num_tables
-    max_num_tables = max(max_num_tables, num_tables)
-    min_num_tables = min(min_num_tables, num_tables)
+    num_tables.append(query['num_tables'])
 
-    num_hdfs_scans = query['num_hdfs_scans']
-    sum_num_hdfs_scans += num_hdfs_scans
-    max_num_hdfs_scans = max(max_num_hdfs_scans, num_hdfs_scans)
-    min_num_hdfs_scans = min(min_num_hdfs_scans, num_hdfs_scans)
+    num_hdfs_scans.append(query['num_hdfs_scans'])
 
-    num_output_columns = query['num_output_columns']
-    sum_num_output_columns += num_output_columns
-    max_num_output_columns = max(max_num_output_columns, num_output_columns)
-    min_num_output_columns = min(min_num_output_columns, num_output_columns)
+    num_output_columns.append(query['num_output_columns'])
 
-    num_from_subqueries = query['num_from_subqueries']
-    sum_num_from_subqueries += num_from_subqueries
-    max_num_from_subqueries = max(max_num_from_subqueries, num_from_subqueries)
-    min_num_from_subqueries = min(min_num_from_subqueries, num_from_subqueries)
+    num_from_subqueries.append(query['num_from_subqueries'])
+
+    runtime.append(query['runtime'] / 1000000000)
 
     try:
-        num_group_by_columns = query['num_group_by_columns']
+        num_group_by_columns.append(query['num_group_by_columns'])
     except KeyError:
-        num_group_by_columns = 0
-    sum_num_group_by_columns += num_group_by_columns
-    max_num_group_by_columns = max(max_num_group_by_columns, num_group_by_columns)
-    min_num_group_by_columns = min(min_num_group_by_columns, num_group_by_columns)
+        num_group_by_columns.append(0)
 
     try:
-        num_order_by_columns = query['num_order_by_columns']
+        num_order_by_columns.append(query['num_order_by_columns'])
     except KeyError:
-        num_order_by_columns = 0
-    sum_num_order_by_columns += num_order_by_columns
-    max_num_order_by_columns = max(max_num_order_by_columns, num_order_by_columns)
-    min_num_order_by_columns = min(min_num_order_by_columns, num_order_by_columns)
+        num_order_by_columns.append(0)
 
     if 'limit' in query:
         num_limit += 1
 
-num_queries = queries.count()
+min_num_joins = min(num_joins)
+max_num_joins = max(num_joins)
+avg_num_joins = sum(num_joins) / float(num_queries)
+hist(num_joins, min_num_joins, max_num_joins,
+        "Number of Joins", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_joins, max_num_joins, avg_num_joins),
+        "num_joins.png")
 
-avg_num_joins = sum_num_joins / float(num_queries)
-print 'avg_num_joins %s' % avg_num_joins
-print 'max_num_joins %s' % max_num_joins
-print 'min_num_joins %s' % min_num_joins
+min_num_broadcast_joins = min(num_broadcast_joins)
+max_num_broadcast_joins = max(num_broadcast_joins)
+avg_num_broadcast_joins = sum(num_broadcast_joins) / float(num_queries)
+hist(num_broadcast_joins, min_num_broadcast_joins, max_num_broadcast_joins,
+        "Number of Broadcast Joins", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_broadcast_joins, max_num_broadcast_joins, avg_num_broadcast_joins),
+        "num_broadcast_joins.png")
 
-avg_num_broadcast_joins = sum_num_broadcast_joins / float(num_queries)
-print 'avg_num_broadcast_joins %s' % avg_num_broadcast_joins
-print 'max_num_broadcast_joins %s' % max_num_broadcast_joins
-print 'min_num_broadcast_joins %s' % min_num_broadcast_joins
+min_num_partitioned_joins = min(num_partitioned_joins)
+max_num_partitioned_joins = max(num_partitioned_joins)
+avg_num_partitioned_joins = sum(num_partitioned_joins) / float(num_queries)
+hist(num_partitioned_joins, min_num_partitioned_joins, max_num_partitioned_joins,
+        "Number of Partitioned Joins", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_partitioned_joins, max_num_partitioned_joins, avg_num_partitioned_joins),
+        "num_partitioned_joins.png")
 
-avg_num_partitioned_joins = sum_num_partitioned_joins / float(num_queries)
-print 'avg_num_partitioned_joins %s' % avg_num_partitioned_joins
-print 'max_num_partitioned_joins %s' % max_num_partitioned_joins
-print 'min_num_partitioned_joins %s' % min_num_partitioned_joins
+min_num_inner_joins = min(num_inner_joins)
+max_num_inner_joins = max(num_inner_joins)
+avg_num_inner_joins = sum(num_inner_joins) / float(num_queries)
+hist(num_inner_joins, min_num_inner_joins, max_num_inner_joins,
+        "Number of Inner Joins", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_inner_joins, max_num_inner_joins, avg_num_inner_joins),
+        "num_inner_joins.png")
 
-avg_num_inner_joins = sum_num_inner_joins / float(num_queries)
-print 'avg_num_inner_joins %s' % avg_num_inner_joins
-print 'max_num_inner_joins %s' % max_num_inner_joins
-print 'min_num_inner_joins %s' % min_num_inner_joins
+min_num_tables = min(num_tables)
+max_num_tables = max(num_tables)
+avg_num_tables = sum(num_tables) / float(num_queries)
+hist(num_tables, min_num_tables, max_num_tables,
+        "Number of Tables", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_tables, max_num_tables, avg_num_tables),
+        "num_tables.png")
 
-avg_num_tables = sum_num_tables / float(num_queries)
-print 'avg_num_tables %s' % avg_num_tables
-print 'max_num_tables %s' % max_num_tables
-print 'min_num_tables %s' % min_num_tables
+min_num_hdfs_scans = min(num_hdfs_scans)
+max_num_hdfs_scans = max(num_hdfs_scans)
+avg_num_hdfs_scans = sum(num_hdfs_scans) / float(num_queries)
+hist(num_hdfs_scans, min_num_hdfs_scans, max_num_hdfs_scans,
+        "Number of HDFS Scans", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_hdfs_scans, max_num_hdfs_scans, avg_num_hdfs_scans),
+        "num_hdfs_scans.png")
 
-avg_num_hdfs_scans = sum_num_hdfs_scans / float(num_queries)
-print 'avg_num_hdfs_scans %s' % avg_num_hdfs_scans
-print 'max_num_hdfs_scans %s' % max_num_hdfs_scans
-print 'min_num_hdfs_scans %s' % min_num_hdfs_scans
+min_num_output_columns = min(num_output_columns)
+max_num_output_columns = max(num_output_columns)
+avg_num_output_columns = sum(num_output_columns) / float(num_queries)
+hist(num_output_columns, min_num_output_columns, max_num_output_columns,
+        "Number of Output Columns", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_output_columns, max_num_output_columns, avg_num_output_columns),
+        "num_output_columns.png")
 
-avg_num_output_columns = sum_num_output_columns / float(num_queries)
-print 'avg_num_output_columns %s' % avg_num_output_columns
-print 'max_num_output_columns %s' % max_num_output_columns
-print 'min_num_output_columns %s' % min_num_output_columns
+min_num_group_by_columns = min(num_group_by_columns)
+max_num_group_by_columns = max(num_group_by_columns)
+avg_num_group_by_columns = sum(num_group_by_columns) / float(num_queries)
+hist(num_group_by_columns, min_num_group_by_columns, max_num_group_by_columns,
+        "Number of Group By Columns", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_group_by_columns, max_num_group_by_columns, avg_num_group_by_columns),
+        "num_group_by_columns.png")
 
-avg_num_group_by_columns = sum_num_group_by_columns / float(num_queries)
-print 'avg_num_group_by_columns %s' % avg_num_group_by_columns
-print 'max_num_group_by_columns %s' % max_num_group_by_columns
-print 'min_num_group_by_columns %s' % min_num_group_by_columns
+min_num_order_by_columns = min(num_order_by_columns)
+max_num_order_by_columns = max(num_order_by_columns)
+avg_num_order_by_columns = sum(num_order_by_columns) / float(num_queries)
+hist(num_order_by_columns, min_num_order_by_columns, max_num_order_by_columns,
+        "Number of Order By Columns", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_order_by_columns, max_num_order_by_columns, avg_num_order_by_columns),
+        "num_order_by_columns.png")
 
-avg_num_order_by_columns = sum_num_order_by_columns / float(num_queries)
-print 'avg_num_order_by_columns %s' % avg_num_order_by_columns
-print 'max_num_order_by_columns %s' % max_num_order_by_columns
-print 'min_num_order_by_columns %s' % min_num_order_by_columns
+min_hdfs_scan_size = min(hdfs_scan_size)
+max_hdfs_scan_size = max(hdfs_scan_size)
+avg_hdfs_scan_size = sum(hdfs_scan_size) / float(num_queries)
+hist(hdfs_scan_size, min_hdfs_scan_size, max_hdfs_scan_size,
+        "HDFS Scan Size (MB)", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_hdfs_scan_size, max_hdfs_scan_size, avg_hdfs_scan_size),
+        "hdfs_scan_size.png")
 
-avg_hdfs_scan_size = sum_hdfs_scan_size / float(num_queries)
-print 'avg_hdfs_scan_size %s' % avg_hdfs_scan_size
-print 'max_hdfs_scan_size %s' % max_hdfs_scan_size
-print 'min_hdfs_scan_size %s' % min_hdfs_scan_size
+min_num_from_subqueries = min(num_from_subqueries)
+max_num_from_subqueries = max(num_from_subqueries)
+avg_num_from_subqueries = sum(num_from_subqueries) / float(num_queries)
+hist(num_from_subqueries, min_num_from_subqueries, max_num_from_subqueries,
+        "Number of From Subqueries", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_num_from_subqueries, max_num_from_subqueries, avg_num_from_subqueries),
+        "num_from_subqueries.png")
 
-avg_num_from_subqueries = sum_num_from_subqueries / float(num_queries)
-print 'avg_num_from_subqueries %s' % avg_num_from_subqueries
-print 'max_num_from_subqueries %s' % max_num_from_subqueries
-print 'min_num_from_subqueries %s' % min_num_from_subqueries
+min_runtime = min(runtime)
+max_runtime = max(runtime)
+avg_runtime = sum(runtime) / float(num_queries)
+hist(runtime, min_runtime, max_runtime,
+        "Runtime", "Number of Queries",
+        "$min = %s$ $max = %s$ $avg = %s$" %
+        (min_runtime, max_runtime, avg_runtime),
+        "runtime.png")
 
 print 'percent_limit %s%%' % (num_limit / float(num_queries) * 100)
